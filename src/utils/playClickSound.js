20f82d46a1ddfe528a17fd088f913dea64442b70
Karl <krf0821@gmail.com>
2025-07-20 09:36:26 +0100

    Lowered volume

--- src/utils/playClickSound.js
+++ src/utils/playClickSound.js
@@ -3,12 +3,12 @@ const mouseUpSound = new Audio(require('../assets/mouseUp.mp3'));
 
 export function playMouseDownSound() {
   mouseDownSound.currentTime = 0;
-  mouseDownSound.volume = 0.5;
+  mouseDownSound.volume = 0.3;
   mouseDownSound.play();
 }
 
 export function playMouseUpSound() {
   mouseUpSound.currentTime = 0;
-  mouseUpSound.volume = 0.5;
+  mouseUpSound.volume = 0.3;
   mouseUpSound.play();
 }
