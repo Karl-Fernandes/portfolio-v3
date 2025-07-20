b106a45d58fb1500c3f15efce859f4cb784b96b4
Karl <krf0821@gmail.com>
2025-07-20 09:33:57 +0100

    Added draggable icons

--- src/components/Shortcuts.js
+++ src/components/Shortcuts.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import styled from 'styled-components'
 import { Icon } from '@react95/core'
-import {startWebamp} from '../utils/startWebamp';
+import { startWebamp } from '../utils/startWebamp'
+import Draggable from 'react-draggable' // Add this import
 
 const StyledShorcut = styled.div`
     margin-left: 20px;
@@ -12,30 +13,36 @@ const StyledShorcut = styled.div`
 function Shortcuts({ openExplorer, openBin }) {
     return (
         <div>
-            <StyledShorcut>
-                <Icon
-                    className="pointer"
-                    name="windows_explorer"
-                    onClick={() => openExplorer()}
-                />
-                <div>Explorer</div>
-            </StyledShorcut>
-            <StyledShorcut>
-                <Icon
-                    className="pointer"
-                    name="media_cd"
-                    onClick={()=>startWebamp()}
-                />
-                <div>Media</div>
-            </StyledShorcut>
-            <StyledShorcut>
-                <Icon
-                    className="pointer"
-                    name="appwiz_1503"
-                    onClick={()=>openBin()}
-                />
-                <div style={{marginLeft:'6px'}}>Bin</div>
-            </StyledShorcut>
+            <Draggable>
+                <StyledShorcut>
+                    <Icon
+                        className="pointer"
+                        name="windows_explorer"
+                        onClick={() => openExplorer()}
+                    />
+                    <div>Explorer</div>
+                </StyledShorcut>
+            </Draggable>
+            <Draggable>
+                <StyledShorcut>
+                    <Icon
+                        className="pointer"
+                        name="media_cd"
+                        onClick={() => startWebamp()}
+                    />
+                    <div>Media</div>
+                </StyledShorcut>
+            </Draggable>
+            <Draggable>
+                <StyledShorcut>
+                    <Icon
+                        className="pointer"
+                        name="appwiz_1503"
+                        onClick={() => openBin()}
+                    />
+                    <div style={{ marginLeft: '6px' }}>Bin</div>
+                </StyledShorcut>
+            </Draggable>
         </div>
     )
 }
